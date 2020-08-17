f501782f45310f311b66be4913873f3aa653fc0a
Abdullah AlGhamdi <50756573+AbdullahMu@users.noreply.github.com>
2020-08-17 17:55:42 +0300

    a commit

---
+++ create_tables.py
@@ -0,0 +1,47 @@
+import configparser
+import psycopg2
+from sql_queries import create_table_queries, drop_table_queries
+
+def drop_tables(cur, conn):
+    """drop database tables from drop_table_queries, 
+    a list with DROP statements
+    """
+    for query in drop_table_queries:
+        print('Executing drop: '+query)
+        cur.execute(query)
+        conn.commit()
+
+def create_tables(cur, conn):
+    """ create database tables from create_table_queries, 
+    a list with INSERT statements
+    """
+    for query in create_table_queries:
+        print('Executing create: '+query)
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    """
+    Extract songs metadata and user activity data from S3, transform it using a staging table, and load it into dimensional tables for analysis
+    """
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    print('Connecting to redshift')
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    print('Connected to redshift')
+    cur = conn.cursor()
+
+    print('Dropping existing tables if any')
+    drop_tables(cur, conn)
+    
+    print('Creating tables')
+    create_tables(cur, conn)
+
+    conn.close()
+    print('Create table Ended')
+
+
+if __name__ == "__main__":
+    main()
